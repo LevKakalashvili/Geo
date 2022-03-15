1c7316328e4093f225c0b257cf89e48c96f85228
Lev <kakalashvililev@yandex.ru>
2022-03-15 18:09:54 +0300

    Добавил запуск сохранения товаров из МойСклад в БД

--- Sync_app/run_sync.py
+++ Sync_app/run_sync.py
@@ -1,3 +1,18 @@
+import os
+import django
+
 from Sync_app import ms
 
-ms.get_assortment()
+from Sync_app.moysklad.moysklad_class_lib import Good as ms_Good
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Geo.settings")
+django.setup()
+
+from Sync_app.models import Good as db_Good
+
+if __name__ == '__main__':
+
+    # Получаем ассортимент
+    goods = ms.get_assortment()
+    # Обновляем БД объектами ассортимента
+    db_Good.create_and_save_objects_from_list(list_ms_goods=goods)
