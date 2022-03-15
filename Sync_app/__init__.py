6788065c5bb58e46fe1a65fc4791643cf60f76a8
Lev <kakalashvililev@yandex.ru>
2022-03-15 18:10:45 +0300

    Запрос токена каждый раз при подключении к сервису МойСклад

--- Sync_app/__init__.py
+++ Sync_app/__init__.py
@@ -2,4 +2,4 @@ from Sync_app.moysklad.moysklad_class_lib import MoySklad
 
 # Получаем токен для работы с сервисом МойСклад
 ms = MoySklad()
-ms.set_token(request_new=False)
+ms.set_token(request_new=True)
