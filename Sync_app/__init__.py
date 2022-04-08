8bdc8cf3d341f4fada85712f720ac1c455c9af88
Lev <kakalashvililev@yandex.ru>
2022-04-08 14:43:47 +0300

    Добавил логин в Контур.Маркете

--- Sync_app/__init__.py
+++ Sync_app/__init__.py
@@ -1,5 +1,11 @@
+"""Инициализация приложени."""
+from Sync_app.konturmarket.konturmarket_class_lib import KonturMarket
 from Sync_app.moysklad.moysklad_class_lib import MoySklad
 
 # Получаем токен для работы с сервисом МойСклад
 ms = MoySklad()
 ms.set_token(request_new=True)
+
+# Создаем инстанс сервиса
+kmarket = KonturMarket()
+kmarket.login()
