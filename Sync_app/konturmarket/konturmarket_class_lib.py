dd3a3a8975602150ed2974170a85cae39493874f
Lev <kakalashvililev@yandex.ru>
2022-04-08 14:53:45 +0300

    Добавил модуль для работы в Контур.Маркете

---
+++ Sync_app/konturmarket/konturmarket_class_lib.py
@@ -0,0 +1,97 @@
+"""В модуле описаны классы для работы с сервисом Контур.Маркет https://market.kontur.ru/."""
+import json
+from dataclasses import dataclass
+from typing import List, Tuple, Optional
+import requests
+from pydantic import BaseModel, Field
+
+import Sync_app.privatedata.kontrurmarket_privatedata as km_pvdata
+
+from Sync_app.konturmarket.konturmarket_urls import Url, UrlType, get_url
+
+session = requests.Session()
+
+
+class Brewery(BaseModel):
+    """Клас описывает структуру компании производителя, продукции в соответствии с терминами ЕГАИС. Словарь
+    'producer' в JSON представлении."""
+
+    name: str = Field(alias='shortName')
+
+
+class GoodEGAIS(BaseModel):
+    """Клас описывает структуру товара, продукции в соответствии с терминами ЕГАИС.
+    Словарь 'productInfo' в JSON представлении."""
+
+    # ЕГАИС наименование
+    name: str = Field(alias='fullName')
+
+    # Код алкогольной продукции (код АП) в ЕГАИС. Уникальный 19-ти значный код. Если значащих цифр в
+    # коде меньше 19-ти, то вперед дописываются нули. Это при строковом представлении
+    alco_code: str = Field(alias='egaisCode')
+
+    # Емкость тары. Необязательный параметр. Отсутствие этого параметра говорит о том, что товар разливной
+    capacity: Optional[float] = None
+
+    # Описание производителя
+    brewery: Brewery = Field(alias='producer')
+
+    def to_tuple(self) -> Tuple[str, str, str]:
+        """Метод возвращает кортеж вида (ЕГАИС_НАИМЕНОВАНИЕ, ЕГАИС_КОД)."""
+        return self.name, self.alco_code, self.brewery.name
+
+    def get_description(self):
+        """Метод возвращает кортеж с наименованием товара."""
+        return (
+            f'Пивоварня: {self.brewery.name}  '
+            f'Наименование: {self.name}  '
+            f'Код: {self.alco_code}  '
+        )
+
+
+@dataclass()
+class KonturMarket:
+    """Класс описывает работу с сервисом Контур.Маркет https://market.kontur.ru/."""
+
+    # Переменная устанавливается в True, в случае успешного логина в сервисе
+    connection_ok: bool = False
+
+    @staticmethod
+    def get_egais_assortment() -> List[GoodEGAIS]:
+        """Метод возвращает список инстансов GoodEGAIS, полученных из сервиса."""
+
+        goods_list: List[GoodEGAIS] = []
+        url: Url = get_url(UrlType.egais_assortment)
+        response = session.get(url.url)
+
+        goods = dict(response.json()).get('list')
+        # Если получили успешный ответ и есть список товаров
+        if response.ok and goods:
+            # Проходим по всему списку товаров, наименований.
+            for good in goods:
+                # Получаем словарь с информацией о товаре
+                goods_list.append(GoodEGAIS(**good['productInfo']))
+
+            # Сортировка по названию пивоварни
+            goods_list = sorted(goods_list, key=lambda element: element.brewery.name)
+            return goods_list
+        return []
+
+    def login(self) -> bool:
+        """Метод для логина в сервиса Контур.Маркет."""
+        auth_data = {
+            'Login': km_pvdata.USER,
+            'Password': km_pvdata.PASSWORD,
+            'Remember': False,
+        }
+        # Пытаемся залогиниться на сайте
+        url: Url = get_url(UrlType.login)
+        response = session.post(
+            url=url.url,
+            data=json.dumps(auth_data),
+            headers=url.headers,
+            cookies=url.cookies,
+        )
+        self.connection_ok = response.ok
+
+        return response.ok
