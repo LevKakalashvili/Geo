74feae4f93526f657556b9f73a32575d8ee976d5
Lev <kakalashvililev@yandex.ru>
2022-04-12 10:13:34 +0300

    Орографическая ошибка

--- Sync_app/run_sync.py
+++ Sync_app/run_sync.py
@@ -22,6 +22,6 @@ if __name__ == '__main__':
 
     # Получаем ассортимент КонтурМаркет
     km_goods = kmarket.get_egais_assortment()
-    # Обновляем БД объектами справочника Контур.Маркет
+    # Обновляем БД объектами справочника из сервиса Контур.Маркет
     db_km_good.save_objects_to_storage(list_km_goods=km_goods)
 
