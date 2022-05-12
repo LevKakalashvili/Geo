7eb262f182b77a2316792106600d6df9bce72687
Lev <kakalashvililev@yandex.ru>
2022-05-12 08:28:27 +0300

    Добавил импорты

--- Sync_app/models/__init__.py
+++ Sync_app/models/__init__.py
@@ -1,2 +1,4 @@
 from Sync_app.models.moysklad_models import *
 from Sync_app.models.konturmarket_models import *
+from Sync_app.models.app_models_func import *
+from Sync_app.models.app_models import *
