e372239fd158007dbbacf5acc950c0c60daa0e13
Lev <kakalashvililev@yandex.ru>
2022-05-17 19:19:32 +0300

    Поправил импорты, убрал код для отладки, поправил докстринги

--- Sync_app/konturmarket/konturmarket_class_lib.py
+++ Sync_app/konturmarket/konturmarket_class_lib.py
@@ -1,20 +1,25 @@
 """В модуле описаны классы для работы с сервисом Контур.Маркет https://market.kontur.ru/."""
 import json
 from dataclasses import dataclass
-from typing import List, Tuple, Optional
+from typing import List
+from typing import Optional
+from typing import Tuple
+
 import requests
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
+from pydantic import Field
 
 import Sync_app.privatedata.kontrurmarket_privatedata as km_pvdata
+from Sync_app.konturmarket.konturmarket_urls import Url
+from Sync_app.konturmarket.konturmarket_urls import UrlType
+from Sync_app.konturmarket.konturmarket_urls import get_url
 
-from Sync_app.konturmarket.konturmarket_urls import Url, UrlType, get_url
 
 session = requests.Session()
 
 
 class Brewery(BaseModel):
-    """Класс описывает структуру компании производителя, продукции в соответствии с терминами ЕГАИС. Словарь
-    'producer' в JSON представлении."""
+    """Класс описывает структуру компании производителя, продукции в соответствии с терминами ЕГАИС. Словарь 'producer' в JSON представлении."""
 
     # Короткое наименование производителя
     short_name: str = Field(alias='shortName')
@@ -27,34 +32,31 @@ class Brewery(BaseModel):
 
 
 class GoodEGAIS(BaseModel):
-    """Класс описывает структуру товара, продукции в соответствии с терминами ЕГАИС.
-    Словарь 'productInfo' в JSON представлении."""
+    """Класс описывает структуру товара, продукции в соответствии с терминами ЕГАИС. Словарь 'productInfo' в JSON представлении."""
 
     # ЕГАИС наименование
     name: str = Field(alias='fullName')
 
     # Код алкогольной продукции (код АП) в ЕГАИС. Уникальный 19-ти значный код. Если значащих цифр в
-    # коде меньше 19-ти, то вперед дописываются нули. Это при строковом представлении
+    # коде меньше 19-ти, то вперед дописываются нули. Это при строковом
+    # представлении
     alco_code: str = Field(alias='egaisCode')
 
-    # Емкость тары. Не обязательный параметр. Отсутствие этого параметра говорит о том, что товар разливной
+    # Емкость тары. Не обязательный параметр. Отсутствие этого параметра
+    # говорит о том, что товар разливной
     capacity: Optional[float] = None
 
     # Описание производителя
     brewery: Brewery = Field(alias='producer')
 
-
     def to_tuple(self) -> Tuple[str, str, str]:
         """Метод возвращает кортеж вида (ЕГАИС_НАИМЕНОВАНИЕ, ЕГАИС_КОД)."""
         return self.name, self.alco_code, self.brewery.short_name
 
-    def get_description(self):
+    def get_description(self) -> str:
         """Метод возвращает кортеж с наименованием товара."""
-        return (
-            f'Пивоварня: {self.brewery.short_name}  '
-            f'Наименование: {self.name}  '
-            f'Код: {self.alco_code}  '
-        )
+        return f'Пивоварня: {self.brewery.short_name}  Наименование: {self.name}  Код: {self.alco_code} '
+
 
 class StockEGAIS(BaseModel):
     """Класс описывает структуру остатка товара, продукции в соответствии с терминами ЕГАИС."""
@@ -69,7 +71,6 @@ class StockEGAIS(BaseModel):
     good: GoodEGAIS = Field(alias='productInfo')
 
 
-
 @dataclass()
 class KonturMarket:
     """Класс описывает работу с сервисом Контур.Маркет https://market.kontur.ru/."""
@@ -80,7 +81,6 @@ class KonturMarket:
     @staticmethod
     def get_egais_assortment() -> List[GoodEGAIS]:
         """Метод возвращает список инстансов GoodEGAIS, полученных из сервиса."""
-
         goods_list: List[GoodEGAIS] = []
         url: Url = get_url(UrlType.EGAIS_ASSORTMENT)
         response = session.get(url.url)
@@ -94,7 +94,9 @@ class KonturMarket:
                 goods_list.append(StockEGAIS(**good))
 
             # Сортировка по названию пивоварни
-            goods_list = sorted(goods_list, key=lambda element: element.good.brewery.short_name)
+            goods_list = sorted(
+                goods_list,
+                key=lambda element: element.good.brewery.short_name)
             return goods_list
         return []
 
