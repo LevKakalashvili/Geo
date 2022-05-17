2a8f989434493bf4eeca4eb4013dfbebdfb7ba21
Lev <kakalashvililev@yandex.ru>
2022-05-17 17:49:10 +0300

    Поправил нотацию типов, исправил импорты, убрал код для отладки

--- Sync_app/common/functions.py
+++ Sync_app/common/functions.py
@@ -1,15 +1,15 @@
 """Модуль для вспомогательных функций."""
 import string
+from typing import Any
 
 
-def string_title(_str: str) -> str:
-    """Функция отрабатывает так же как string.title(), но учитывает, что после апострофа должен идти символ в нижнем
-    регистре."""
+def string_title(_str: Any) -> str:
+    """Функция отрабатывает так же как string.title(), но учитывает, что после апострофа должен идти символ в нижнем регистре.""" # pylint: disable=line-too-long
     # Т.к. str.title() не корректно обрабатывает апостроф
-    if type(_str) is list and len(_str) == 1:
+    if isinstance(_str, list) and len(_str) == 1:
         _str = _str[0]
 
-    if '\'' in _str:
+    if "'" in _str:
         _str = string.capwords(_str)
     else:
         _str = _str.title()
