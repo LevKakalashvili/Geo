e25acadc747c06e368cefa43ecf8cc074df8b1bf
Lev <kakalashvililev@yandex.ru>
2022-05-17 17:41:27 +0300

    Поправил импорты

--- Sync_app/models/__init__.py
+++ Sync_app/models/__init__.py
@@ -1,4 +1,5 @@
-from Sync_app.models.moysklad_models import *
-from Sync_app.models.konturmarket_models import *
-from Sync_app.models.app_models_func import *
+"""Инициализация приложения."""
 from Sync_app.models.app_models import *
+from Sync_app.models.app_models_func import *
+from Sync_app.models.konturmarket_models import *
+from Sync_app.models.moysklad_models import *
