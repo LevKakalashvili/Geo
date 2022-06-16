d477e0d16fe8558e4e0f90d46e25050006f64048
Lev <kakalashvililev@yandex.ru>
2022-06-16 14:45:58 +0300

    Исправил команду

--- Sync_app/management/commands/do_sync.py
+++ Sync_app/management/commands/do_sync.py
@@ -1,23 +1,30 @@
 """Команда запуска синхронизации с МойСклад, КонтурМаркет, GoogleSheets."""
-from django.core.management.base import BaseCommand
+from typing import Dict, Tuple, Any
+
+from django.core.management.base import BaseCommand, CommandParser
+
 import Sync_app.moysklad.moysklad_class_lib as ms_class
 
-# from typing import TYPE_CHECKING
-#
-# if TYPE_CHECKING:
-#     import Sync_app.moysklad.moysklad_class_lib as ms_class
 
+class Command(BaseCommand):  # noqa: D101
+    help = "Заполнение БД из сервисов МойСклад, КонтурМаркет. "  # noqa
 
-class Command(BaseCommand):
-    help = "Запуск синхронизации."
+    def add_arguments(self, parser: CommandParser) -> None:  # noqa: D102
 
-    def add_arguments(self, parser):
-        parser.add_argument('-ms_assort', '--moysklad_assortment', action='store_true', help='Запустить синхронизацию ассортимента')
+        parser.add_argument(
+            "-ma",
+            "--moysklad_assortment",
+            action="store_true",
+            default=False,
+            help="Запустить синхронизацию ассортимента из МойСклад",
+        )
 
-    def handle(self, *args, **kwargs):
-        moysklad_assortment = kwargs['moysklad_assortment']
+    def handle(self, *args: Tuple[str], **kwargs: Dict[str, Any]) -> None:  # noqa: D102
+
+        moysklad_assortment = kwargs["moysklad_assortment"]
 
         if moysklad_assortment:
             ms: ms_class.MoySklad = ms_class.MoySklad()
+
             if not ms.sync_assortment():
-                self.stdout.write(self.style.ERROR('Ошибка. Не удалось получить данные из сервиса МойСклад.'))
+                self.stdout.write(self.style.ERROR("Ошибка. Не удалось получить данные из сервиса МойСклад."))
