eae6723c4e62702ca98dbf5fd8bfa84522a6bbd0
lev <kakalashvilillev@yandex.ru>
2022-07-16 15:09:44 +0300

    Тест

--- Sync_app/views.py
+++ Sync_app/views.py
@@ -1,20 +1,12 @@
-from operator import itemgetter
-from typing import List, Dict, Union
-
-from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
-
-from Sync_app.models import KonturMarketDBGood, KonturMarketDBStock
-
-"""Модуль содержит описание моделей для работы с сервисом Контур.Маркет."""
 import datetime
+from operator import itemgetter
+from typing import Dict, List
 
-from Sync_app.models.moysklad_models import (
-    MoySkladDBRetailDemand,
-)
-from Sync_app.moysklad.moysklad_constants import GoodType
-
+from django.shortcuts import render
 from django.views.generic import TemplateView
+
+from Sync_app.models.moysklad_models import MoySkladDBRetailDemand
+from Sync_app.moysklad.moysklad_constants import GoodType
 
 
 class EgaisView(TemplateView):
@@ -24,54 +16,47 @@ class EgaisView(TemplateView):
 
         sales: List[Dict[str, str | int]] = []
 
-        date_ = datetime.date.today() - datetime.timedelta(days=1)
-        all_sold_ms_goods = MoySkladDBRetailDemand.objects\
-            .select_related("uuid") \
-            .filter(demand_date__exact=date_)\
-            .filter(uuid__is_draft=False) \
-            .exclude(uuid__bev_type__in=[GoodType.KOMBUCHA, GoodType.OTHER, GoodType.LEMONADE])\
-            .prefetch_related("uuid__egais_code")\
-            .prefetch_related("uuid__egais_code__konturmarketdbstock")\
+        date_ = datetime.date.today() - datetime.timedelta(days=2)
+        all_sold_ms_goods = (
+            MoySkladDBRetailDemand.objects.select_related("uuid")
+            .filter(demand_date__exact=date_)
+            .filter(uuid__is_draft=False)
+            .exclude(uuid__bev_type__in=[GoodType.KOMBUCHA, GoodType.OTHER, GoodType.LEMONADE])
+            .prefetch_related("uuid__egais_code")
+            .prefetch_related("uuid__egais_code__konturmarketdbstock")
             .filter(uuid__egais_code__konturmarketdbstock__quantity__gt=0)
+        )
 
         quantity: int = 0
-        for good in all_sold_ms_goods:
-            quantity = good.quantity
-            # TODO: Необходимо учесть в всех случаях ситуацию, что в ЕГАИС остаток меньше, чем нужно списать
 
-            if len(good.uuid.egais_code.all()) < 2:
+        for good in all_sold_ms_goods:
+            for km_good in good.uuid.egais_code.all():
+                # Если остаток товара в ЕГАИС = 0
+                if not km_good.konturmarketdbstock.quantity:
+                    continue
+
+                # Если товара в ЕГАИС больше, чем продано
+                if km_good.konturmarketdbstock.quantity >= good.quantity:
+                    quantity = good.quantity
+                    good.quantity = 0
+                    km_good.konturmarketdbstock.quantity -= good.quantity
+                # Если товара в ЕГАИС меньше, чем продано
+                elif km_good.konturmarketdbstock.quantity < good.quantity:
+                    quantity = km_good.konturmarketdbstock.quantity
+                    good.quantity -= km_good.konturmarketdbstock.quantity
+                    km_good.konturmarketdbstock.quantity = 0
+
                 sales.append(
                     {
                         "commercial_name": good.uuid.full_name,
-                        "name": good.uuid.egais_code.all()[0].full_name,
-                        "alcCode": good.uuid.egais_code.all()[0].egais_code,
-                        "apCode": good.uuid.egais_code.all()[0].kind_code,
-                        "volume": good.uuid.egais_code.all()[0].capacity,
-                        # если в БД остаток товара меньше реально проданного, то списываем товар под ноль
-                        "quantity": quantity,  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                        "name": km_good.full_name,
+                        "alcCode": km_good.egais_code,
+                        "apCode": km_good.kind_code,
+                        "volume": km_good.capacity,
+                        "quantity": quantity,
                         "price": good.uuid.price,
                     },
                 )
-            else:
-                for km_good in good.uuid.egais_code.all():
-                    if km_good.konturmarketdbstock.quantity == 0 or good.quantity == 0:
-                        break
-                    else:
-                        quantity = good.quantity
-                        good.quantity = 0
-
-                    sales.append(
-                        {
-                            "commercial_name": good.uuid.full_name,
-                            "name": good.uuid.egais_code.all()[0].full_name,
-                            "alcCode": km_good.egais_code,
-                            "apCode": km_good.kind_code,
-                            "volume": km_good.capacity,
-                            # если в БД остаток товара меньше реально проданного, то списываем товар под ноль
-                            "quantity": quantity,
-                            "price": good.uuid.price,
-                        },
-                    )
 
         sales = sorted(sales, key=itemgetter("commercial_name"))
 
