af65815bea58d3cd5c1c60a67c049bcaa1a991b4
lev <kakalashvililev@yandex.ru>
2022-07-18 16:58:43 +0300

    add Russian names

--- Geo/settings.py
+++ Geo/settings.py
@@ -121,7 +121,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
